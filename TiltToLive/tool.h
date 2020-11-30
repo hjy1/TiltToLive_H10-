d7e8a8ad227e9d6ccf8682a3d7e27b379fdd300a
dongdong3272 <74498933+dongdong3272@users.noreply.github.com>
2020-12-01 02:34:08 +0800

    fix a typo, add some member functions to tool

--- TiltToLive/tool.h
+++ TiltToLive/tool.h
@@ -10,18 +10,19 @@ class Tool: public Object{
 private:
     const Object* const arrow_ptr;
     QGraphicsPixmapItem* item;
-    enum class ToolType {INVINCE, FRRRZE, SHOOT, BOOM, EXPLOSION, SWIRL, SHOCKWAVE} toolType; //seven tools for now
-    double rotate_angle{0};
+    enum class ToolType {INVINCE, FREEZE, SHOOT, BOOM, EXPLOSION, SWIRL, SHOCKWAVE} toolType; //seven tools for now
+    int time_count = 0;
+    double rotate_angle = 0;
     static const int TYPENUM;// = 7;
     static const QString tool_image_lookup[];
-    /* = {  ":/resources/tools/invince.jpg",
-            ":/resources/tools/freeze.jpg",
-            ":/resources/tools/shoot.jpg",
-            ":/resources/tools/boom.jpg",
-            ":/resources/tools/explosion.jpg",
-            ":/resources/tools/swirl.jpg",
-            ":/resources/tools/shockwave.jpg",
-         };*/
+        /* = {  ":/resources/tools/invince.png",
+                ":/resources/tools/freeze.png",
+                ":/resources/tools/shoot.png",
+                ":/resources/tools/boom.png",
+                ":/resources/tools/explosion.png",
+                ":/resources/tools/swirl.png",
+                ":/resources/tools/shockwave.png"
+             };*/
 
     void set_item_position();
 
@@ -32,14 +33,17 @@ public:
     /* Create a tool on random position in game map
      * will call Object(const double &r);
      * ********************************************** */
-    Tool(QGraphicsScene* scene = nullptr, const Object* const &target = nullptr);
+    Tool(QGraphicsScene* scene = nullptr, const Object* const &arrow = nullptr);
 
     ~Tool();
 
+    void get_initial_v();
     void reset_v();
     void add_scene(QGraphicsScene* scene);
     void move_one_tick();
     void rotate();
+    void clock_change();
+    int get_time() const;
     bool arrow_reached() const;
 
     void operation();
